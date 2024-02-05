489ea3c6150d14b4606b0fda614fc816d4852edd
Olaru-Petru <olarustefan0135@yahoo.com>
2024-02-05 18:57:43 +0200

    Add project files.

---
+++ Data/MoviesNETContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoviesNET.Models;
+
+namespace MoviesNET.Data
+{
+    public class MoviesNETContext : DbContext
+    {
+        public MoviesNETContext (DbContextOptions<MoviesNETContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoviesNET.Models.Movie> Movie { get; set; } = default!;
+    }
+}
